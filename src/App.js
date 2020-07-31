cd92ad1905c5a43af5dc15e2a8f39cf42d70e02b
Yashpalkhandelwal <yashkumar1235pal@gmail.com>
2020-07-31 14:28:45 +0530

    Clean up done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+    <div className="app">
+      <Switch>
+        <Router path= "/checkout">
+          <h1>checkout</h1>
+        </Router>
+        <Router path= "/login">
+          <h1>LOG IN</h1>
+        </Router>
+        <Router path= "/">
+          <h1>HOME PAGE</h1>
+        </Router>
+
+        </Switch>
     </div>
+    </Router>
   );
 }
 
---
+++ src/key.js
@@ -0,0 +1,19 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCD_P3K8yc7g5BuKcy26gdFUWD2sYTbaGk",
+  authDomain: "clone-e4d1f.firebaseapp.com",
+  databaseURL: "https://clone-e4d1f.firebaseio.com",
+  projectId: "clone-e4d1f",
+  storageBucket: "clone-e4d1f.appspot.com",
+  messagingSenderId: "548308296080",
+  appId: "1:548308296080:web:c7d3415307bbfec4c15315",
+  measurementId: "G-HFZ0CH9CJE"
+};const firebaseConfig = {
+  apiKey: "AIzaSyCD_P3K8yc7g5BuKcy26gdFUWD2sYTbaGk",
+  authDomain: "clone-e4d1f.firebaseapp.com",
+  databaseURL: "https://clone-e4d1f.firebaseio.com",
+  projectId: "clone-e4d1f",
+  storageBucket: "clone-e4d1f.appspot.com",
+  messagingSenderId: "548308296080",
+  appId: "1:548308296080:web:c7d3415307bbfec4c15315",
+  measurementId: "G-HFZ0CH9CJE"
+};
