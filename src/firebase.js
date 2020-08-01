4949e7b22ee853a54df742a57a5281ddf9fb4263
Yashpalkhandelwal <yashkumar1235pal@gmail.com>
2020-08-01 14:53:50 +0530

    firebase initialized

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,7 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyCD_P3K8yc7g5BuKcy26gdFUWD2sYTbaGk",
+import firebase from "firebase";
+
+const firebaseApp = firebase.initializeApp({
+     apiKey: "AIzaSyCD_P3K8yc7g5BuKcy26gdFUWD2sYTbaGk",
   authDomain: "clone-e4d1f.firebaseapp.com",
   databaseURL: "https://clone-e4d1f.firebaseio.com",
   projectId: "clone-e4d1f",
@@ -7,13 +9,8 @@ const firebaseConfig = {
   messagingSenderId: "548308296080",
   appId: "1:548308296080:web:c7d3415307bbfec4c15315",
   measurementId: "G-HFZ0CH9CJE"
-};const firebaseConfig = {
-  apiKey: "AIzaSyCD_P3K8yc7g5BuKcy26gdFUWD2sYTbaGk",
-  authDomain: "clone-e4d1f.firebaseapp.com",
-  databaseURL: "https://clone-e4d1f.firebaseio.com",
-  projectId: "clone-e4d1f",
-  storageBucket: "clone-e4d1f.appspot.com",
-  messagingSenderId: "548308296080",
-  appId: "1:548308296080:web:c7d3415307bbfec4c15315",
-  measurementId: "G-HFZ0CH9CJE"
-};
+});
+
+const auth = firebase.auth();
+
+export { auth };
