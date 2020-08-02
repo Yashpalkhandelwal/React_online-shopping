0f6b8f95a14a21f38089dfabf808c4a7481b2d9e
Yashpalkhandelwal <yashkumar1235pal@gmail.com>
2020-08-02 14:47:56 +0530

    ADD ?CHECKOUT ? LOGIN DONE

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Header from "./Header";
+import Home from "./Home";
+import Checkout from './Checkout';
+import Login from './Login';
+import { useStateValue } from './StateProvider';
+import { auth } from "./firebase";
 
 function App() {
+  const [{ user }, dispatch] = useStateValue();
+  // useEffect <- POWERFUL
+  // piece of code that runs based on a give condition
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((authUser) => {
+      if(authUser){
+        // the user is logged in
+        dispatch({
+          type: 'SET_USER',
+          user: authUser
+        });
+      } else {
+        // the user is logged out
+        dispatch({
+          type: 'SET_USER',
+          user: null
+        });
+      }
+    });
+
+    return () => {
+      // any clean up operations go here
+      unsubscribe();
+    };
+  }, []);
   return (
     <Router>
-    <div className="app">
-      <Switch>
-        <Route path= "/checkout">
-          <h1>checkout</h1>
-        </Route>
-        <Route path= "/login">
-          <h1>LOG IN</h1>
-        </Route>
-        <Route path= "/">
-        <Header />
-          <h1>HOME PAGE</h1>
-        </Route>
-
+      <div className="app">
+        <Switch>
+          <Route path='/checkout'>
+            <Header />
+            <Checkout />
+          </Route>
+          <Route path='/login'>
+            <Login />
+          </Route>
+          {/* This is the default route */}
+          <Route path='/'>
+            <Header />
+            <Home /> 
+          </Route>
         </Switch>
-    </div>
+      </div>
     </Router>
   );
 }
--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,14 @@
 import firebase from "firebase";
 
 const firebaseApp = firebase.initializeApp({
-     apiKey: "AIzaSyCD_P3K8yc7g5BuKcy26gdFUWD2sYTbaGk",
-  authDomain: "clone-e4d1f.firebaseapp.com",
-  databaseURL: "https://clone-e4d1f.firebaseio.com",
-  projectId: "clone-e4d1f",
-  storageBucket: "clone-e4d1f.appspot.com",
-  messagingSenderId: "548308296080",
-  appId: "1:548308296080:web:c7d3415307bbfec4c15315",
-  measurementId: "G-HFZ0CH9CJE"
+    apiKey: "AIzaSyBIGpnhJr2OlhSYa7ethhonc-OTW9VhoVA",
+    authDomain: "clone-1e5f0.firebaseapp.com",
+    databaseURL: "https://clone-1e5f0.firebaseio.com",
+    projectId: "clone-1e5f0",
+    storageBucket: "clone-1e5f0.appspot.com",
+    messagingSenderId: "706546941030",
+    appId: "1:706546941030:web:c9e4fb6a540b21ce23c1a7",
+    measurementId: "G-JSF4K0QH4G"
 });
 
 const auth = firebase.auth();
